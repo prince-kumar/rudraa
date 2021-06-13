5f28ee7d6c4cb1c16db50781fae85604697af955
Piyush55dude <piyushagarwalvo@gmail.com>
2021-06-13 23:09:48 +0530

    Removed Responsive Message

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Home from "./pages/Home/Home";
 function App() {
   return (
     <>
-      <h2 className="message">Site can only be viewed in desktop</h2>
+      {/* <h2 className="message">Site can only be viewed in desktop</h2> */}
       <div className="App">
         <Header />
         <Home />
